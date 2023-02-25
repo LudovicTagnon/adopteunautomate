e75e8492afcbdbddc89df28d6bec142e931d6710
LudovicTagnon <ludovic.tgn@outlook.com>
2023-02-25 03:52:58 +0100

    Toutes petites modifications de style ET Ajout de nouveaux champs à l'entité Utilisateurs

--- src/Entity/Utilisateurs.php
+++ src/Entity/Utilisateurs.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UtilisateursRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
@@ -33,6 +34,64 @@ class Utilisateurs implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->id;
     }
+
+    /**
+     * @ORM\Column(type="string", length=50, options={"default": "Unknown"})
+     */
+    private ?string $nom = "inconnu";
+
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private ?string $prenom = "inconnu";
+
+
+    /**
+     * @ORM\Column(type="string", length=20, options={"default": "Unknown"})
+     */
+    private ?string $num_tel = "Unknown";
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private ?bool $vehicule = false;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     * @Assert\Choice({"homme", "femme", "autre"})
+     */
+    private ?string $genre = null;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private ?bool $autorisation_mail = false;
+
+    /**
+     * @ORM\Column(type="blob", nullable=true)
+     */
+    private $fichier_photo = null;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\PositiveOrZero
+     */
+    private ?int $cumul_notes = 0;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\PositiveOrZero
+     */
+    private ?int $nb_notes = 0;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private ?bool $compte_actif = true;
+
+    #[ORM\OneToMany(mappedBy: 'utilisateurs', targetEntity: Groupes::class, orphanRemoval: true)]
+    private $groupes;
 
     public function getEmail(): ?string
     {
@@ -98,4 +157,123 @@ class Utilisateurs implements UserInterface, PasswordAuthenticatedUserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNumTel(): ?string
+    {
+        return $this->num_tel;
+    }
+
+    public function setNumTel(?string $num_tel): self
+    {
+        $this->num_tel = $num_tel;
+
+        return $this;
+    }
+
+    public function getVehicule(): ?bool
+    {
+        return $this->vehicule;
+    }
+
+    public function setVehicule(?bool $vehicule): self
+    {
+        $this->vehicule = $vehicule;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(?string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getAutorisationMail(): ?bool
+    {
+        return $this->autorisation_mail;
+    }
+
+    public function setAutorisationMail(?bool $autorisation_mail): self
+    {
+        $this->autorisation_mail = $autorisation_mail;
+
+        return $this;
+    }
+
+    public function getFichierPhoto()
+    {
+        return $this->fichier_photo;
+    }
+
+    public function setFichierPhoto($fichier_photo): self
+    {
+        $this->fichier_photo = $fichier_photo;
+
+        return $this;
+    }
+    public function getCumulNotes(): ?int
+    {
+        return $this->cumul_notes;
+    }
+
+    public function setCumulNotes(?int $cumul_notes): self
+    {
+        $this->cumul_notes = $cumul_notes;
+
+        return $this;
+    }
+
+    public function getNbNotes(): ?int
+    {
+        return $this->nb_notes;
+    }
+
+    public function setNbNotes(?int $nb_notes): self
+    {
+        $this->nb_notes = $nb_notes;
+
+        return $this;
+    }
+    public function getCompteActif(): ?bool
+    {
+        return $this->compte_actif;
+    }
+
+    public function setCompteActif(?bool $compte_actif): self
+    {
+        $this->compte_actif = $compte_actif;
+
+        return $this;
+    }
+
 }
