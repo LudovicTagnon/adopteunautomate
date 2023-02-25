7512ec21f27bb0c57cb82d3c05ae3975d16ad3dd
LudovicTagnon <ludovic.tgn@outlook.com>
2023-02-25 05:10:46 +0100

    Toutes petites modifications de style ET Ajout de champs dans le formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,12 +5,15 @@ namespace App\Form;
 use App\Entity\Utilisateurs;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class RegistrationFormType extends AbstractType
 {
@@ -18,14 +21,6 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -43,7 +38,51 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-        ;
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'attr' => ['autocomplete' => 'nom'],
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true,
+                'attr' => ['autocomplete' => 'prenom'],
+            ])
+            ->add('num_tel', TextType::class, [
+                'required' => true,
+                'attr' => ['autocomplete' => 'num_tel'],
+            ])
+            ->add('vehicule', CheckboxType::class, [
+                'required' => false,
+                'attr' => ['autocomplete' => 'vehicule'],
+            ])
+            ->add('genre', ChoiceType::class, [
+                'required' => true,
+                'choices' => [
+                    'Male' => 'male',
+                    'Female' => 'female',
+                    'Other' => 'other',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'placeholder' => 'Genre (optionnel)',
+            ])
+
+            ->add('autorisation_mail', CheckboxType::class, [
+                'required' => false,
+                'attr' => ['autocomplete' => 'autorisation_mail'],
+            ])
+            ->add('fichier_photo', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'fichier_photo'],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
