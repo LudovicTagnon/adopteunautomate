a5e45db7a60c8b1bc32ed94d4d5a4c04fddf109a
Maxime <maximerosio1@gmail.com>
2023-02-28 23:34:17 +0100

    Ajout champs modification MDP
    (pas encore fonctionnel) juste visuel

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -9,6 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Form\UserProfileFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Error;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use App\Entity\Utilisateurs;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Config\Security\PasswordHasherConfig;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+
+
+
 
 class ProfilController extends AbstractController
 {
@@ -23,13 +36,23 @@ class ProfilController extends AbstractController
         ]);
     }
 
-    public function editProfile(Request $request,EntityManagerInterface $entityManager, ManagerRegistry $doctrine)
+    public function editProfile(Request $request,EntityManagerInterface $entityManager, ManagerRegistry $doctrine, UserPasswordHasherInterface $userPasswordHasher)
     {
+        //$this->denyAccessUnlessGranted('ROLE_USER'); // Check if user is authenticated
         $user = $this->getUser(); //on obtient user connecté
         $form = $this->createForm(UserProfileFormType::class,$user); //creation formulaire avec données user
-        
+
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            //Verif que l'ancien mdp est correct 
+            $oldPassword = $form->get('oldPassword')->getData();
+            if(!$userPasswordHasher->isPasswordValid($user, $oldPassword)){
+                $form->get('oldPassword')->addError(new FormError('Ancien mot de passe incorrect'));
+            }
+            
+
+            
             $entityManager = $doctrine->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
@@ -41,7 +64,6 @@ class ProfilController extends AbstractController
 
         return $this->render('profil/index.html.twig', ['form' => $form->createView(),]);
     }
-
 
 
    
