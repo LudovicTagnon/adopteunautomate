05e995a3c93f51e46fa028d1766d2412686418db
Maxime <maximerosio1@gmail.com>
2023-03-01 19:39:37 +0100

    Ajout de contraintes sur les champs

--- src/Form/UserProfileFormType.php
+++ src/Form/UserProfileFormType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserProfileFormType extends AbstractType
 {
@@ -20,14 +21,35 @@ class UserProfileFormType extends AbstractType
     {
         $builder
             ->add('email',EmailType::class)
-            ->add('prenom',TextType::class)
+            ->add('prenom',TextType::class,['required' => true,
+            'attr' => ['autocomplete' => 'nom'],
+            'constraints' => [
+                new Assert\Regex([
+                    'pattern' => '/^[A-Za-z\s\-]+$/',
+                    'message' => 'Le nom ne doit contenir que des caractères alphabétiques, des espaces et des tirets.',
+                ]),
+            ],
+            ])
             ->add('nom',TextType::class,[
                 'required' => true,
                 'attr' => ['autocomplete' => 'nom'],
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^[A-Za-z\s\-]+$/',
+                        'message' => 'Le prenom ne doit contenir que des caractères alphabétiques, des espaces et des tirets.',
+                    ]),
+                ],
             ])
+            
             ->add('num_tel',TextType::class,[
                 'required' => true,
                 'attr' => ['autocomplete' => 'num_tel'],
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^0[1-9](\d{2}){4}$/',
+                        'message' => 'Le numéro de téléphone doit être un numéro de téléphone français.',
+                    ]),
+                ],
             ])
             ->add('vehicule',CheckboxType::class,[
                 'required' => false,
