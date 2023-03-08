22c160f27e1e53c339607820174222814361afcc
Alexandre <alexandre.antunes4@etu.univ-lorraine.fr>
2023-03-08 22:05:40 +0100

    Suppression notification fonctionnelle

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -6,7 +6,10 @@ use App\Service\NotificationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Notification;
 class NotificationController extends AbstractController
 {
     #[Route('/notification', name: 'app_notification')]
@@ -19,4 +22,21 @@ class NotificationController extends AbstractController
             'notifications' => $notifications,
         ]);
     }
+    #[Route('/notification/supprimer', name: 'app_supprimer_notif')]
+    public function supprimerNotification(Request $request, EntityManagerInterface $entityManager, ManagerRegistry $doctrine): Response
+    {
+        $notificationId = $request->get('id');
+    
+        $notification = $entityManager->getRepository(Notification::class)->find($notificationId);
+    
+        if (!$notification) {
+            throw $this->createNotFoundException('Notification introuvable');
+        }
+    
+        $entityManager->remove($notification);
+        $entityManager->flush();
+    
+        return $this->redirectToRoute('app_notification');
+    }
+    
 }
