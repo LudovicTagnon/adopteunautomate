287ebbcc08fcb58a344112a1e9100fec73a83b7e
Maxime <maximerosio1@gmail.com>
2023-03-08 21:36:19 +0100

    Modif getter
    modif type retour getter getCreateur

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -6,6 +6,9 @@ use App\Service\NotificationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Notification;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class NotificationController extends AbstractController
 {
@@ -19,4 +22,28 @@ class NotificationController extends AbstractController
             'notifications' => $notifications,
         ]);
     }
+
+    #[Route('/notification', name:'notification_mark_as_read')]
+    public function markNotificationAsRead(NotificationService $notificationService, $id, Request $request): Response
+    {
+        $form = 
+        //Marquer comme lu 
+        $notificationService->markAsRead($id);
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/notification', name:'delete_notification')]
+    public function deleteNotification(NotificationService $notificationService, Request $request,  $notification): Response
+    {
+        //Supprimer la notif
+        if ($this->isCsrfTokenValid('delete_notification', $request->request->get('_token'))) {
+            //$notificationService->deleteNotification($notification);
+        }
+
+        
+
+        return $this->redirectToRoute('app_home');
+    }
+
+    
 }
