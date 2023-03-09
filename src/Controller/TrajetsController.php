5bc2597220ebc8bb34b03dac5edf35e7c5ad5cb5
Maxime <maximerosio1@gmail.com>
2023-03-09 08:03:01 +0100

    restriction accès trajets
    restriction d'accès à la page trajet utilisateurs connectés

--- src/Controller/TrajetsController.php
+++ src/Controller/TrajetsController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 #[Route('/trajets')]
 class TrajetsController extends AbstractController
@@ -20,6 +21,7 @@ class TrajetsController extends AbstractController
     
 
     #[Route('/', name: 'app_trajets_index', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
     public function index(TrajetsRepository $trajetsRepository): Response
     {
         
