a3657a4970c2ddf67e10ae71838a29176ca74ed2
Alexandre <alexandre.antunes4@etu.univ-lorraine.fr>
2023-03-15 21:56:46 +0100

    correction effacement notification quand passager accepte

--- src/Controller/EstAccepteController.php
+++ src/Controller/EstAccepteController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\EstAccepte;
 use App\Entity\Trajets;
 use App\Entity\Utilisateurs;
-use App\Repository\EstAccepteRepository;
+use App\Entity\Notification;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,11 +16,12 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class EstAccepteController extends AbstractController
 {
 
-    #[Route("/notification/estAccepte/{trajetId}/{utilisateurId}", name:"app.estAccepte")]
-    public function accepterUnUtilisateur(EntityManagerInterface $manager, int $trajetId, int $utilisateurId,NotificationService $notificationService,RequestStack $requestStack): Response
+    #[Route("/notification/estAccepte/{trajetId}/{utilisateurId}/{notificationId}", name:"app.estAccepte")]
+    public function accepterUnUtilisateur(EntityManagerInterface $manager, int $trajetId, int $utilisateurId,NotificationService $notificationService,int $notificationId): Response
     {
         $trajet = $manager->getRepository(Trajets::class)->find($trajetId);
         $utilisateur = $manager->getRepository(Utilisateurs::class)->find($utilisateurId);
+        $notification = $manager->getRepository(Notification::class)->find($notificationId);
         if (!$trajet) {
             throw $this->createNotFoundException('Trajet introuvable.');
         }
@@ -39,15 +40,16 @@ class EstAccepteController extends AbstractController
         //$notificationService->addNotificationAdopteTrajet("Vous avez adopté un trajet !", $utilisateur,$trajet); //notification
         $this->addFlash('success', "L'utilisateur ".$utilisateur->getNom(). " a été accepté pour le trajet de : ". $trajet->getDemarreA()." vers ".$trajet->getArriveA());
         // On redirige l'utilisateur à la page où il était en supprimant la notification
-        return $this->redirectToRoute('app_supprimer_notif');//TODO:changer ici
+        return $this->redirectToRoute('app_supprimer_notif', ['id' => $notification->getId()]);
         //return new Response('L\'utilisateur ' . $utilisateur->getNom() . ' a adopté le trajet pour : ' . $trajet->getArriveA());//à revoir la redirection
     }
 
     
-    #[Route("/notification/estRefuse/{trajetId}/{utilisateurId}", name:"app.estRefuse")]
-    public function refuserUnUtilisateur(EntityManagerInterface $manager, int $trajetId, int $utilisateurId,NotificationService $notificationService,RequestStack $requestStack): Response
+    #[Route("/notification/estRefuse/{trajetId}/{utilisateurId}/{notificationId}", name:"app.estRefuse")]
+    public function refuserUnUtilisateur(EntityManagerInterface $manager, int $trajetId, int $utilisateurId,int $notificationId,NotificationService $notificationService): Response
     {
         $trajet = $manager->getRepository(Trajets::class)->find($trajetId);
+        $notification = $manager->getRepository(Notification::class)->find($notificationId);
 
         if (!$trajet) {
             throw $this->createNotFoundException('Trajet introuvable.');
@@ -60,9 +62,9 @@ class EstAccepteController extends AbstractController
         }
         //TODO:envoyer les notifs
         $this->addFlash('success', "L'utilisateur ".$utilisateur->getNom(). " a été refusé pour le trajet de : ". $trajet->getDemarreA()." vers ".$trajet->getArriveA());
-        //TODO:retirer l'utilisateur refusé de adopte ??
+        //TODO:retirer l'utilisateur refusé de adopte ?? oui
         // On redirige l'utilisateur à la page où il était en supprimant la notification
-        return $this->redirectToRoute('app_supprimer_notif');//TODO:changer ici
+        return $this->redirectToRoute('app_supprimer_notif', ['id' => $notification->getId()]);
     }
 
 }
