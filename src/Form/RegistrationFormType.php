90902b53e1fa32e45186de9e8954046d344f16d0
BlueVader54 <91185720+BlueVader54@users.noreply.github.com>
2023-04-04 10:52:53 +0200

    Traduction des messages d'erreur trouvés

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -37,9 +37,13 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit contenir {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                    new Assert\Regex([
+                        'pattern' => '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/',
+                        'message' => 'Le mot de passe doit contenir 8 caractères dont au moins 1 minuscule, 1 majuscule, 1 chiffre et 1 caractère spécial',
                     ]),
                 ],
                 'label' => 'Mot de passe *',
@@ -114,7 +118,7 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les termes.',
                     ]),
                 ],
             ]);
