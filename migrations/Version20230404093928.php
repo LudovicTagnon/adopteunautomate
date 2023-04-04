5fcff9974c2a7acfa85e7a49832347c4b818340a
Alexandre <alexandre.antunes4@etu.univ-lorraine.fr>
2023-04-04 11:45:39 +0200

    test

--- migrations/Version20230404093928.php
+++ migrations/Version20230404093928.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230404074135 extends AbstractMigration
+final class Version20230404093928 extends AbstractMigration
 {
     public function getDescription(): string
     {
